c5e48c97c73626f5a996542302a443ce5462320a
jonkvix <jonkvix@gmail.com>
2016-04-12 14:15:36 -0700

    jquery data retrieval test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import $ from 'jquery';
 import React, { Component } from 'react';
 import EventTeam from './EventTeam';
 
 export class App extends Component {
+
+	getData() {
+		const url = 'http://jsonplaceholder.typicode.com/users';
+		$.get(url, (data) => {console.log(data)})
+	}
+
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+
+		this.getData();
+	}
+
+
+
+
   render() {
     return (
       <EventTeam />
